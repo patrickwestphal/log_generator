cba409d39d484767f371a337a0f27e60263746e2
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2018-10-04 17:38:16 +0200

    Project structure initialization

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='log_generator',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Patrick Westphal',
+    author_email='',
+    description='',
+    install_requires=[
+        'rdflib==4.2.2'
+    ]
+)
