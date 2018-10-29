6dd8d4a808f8f196770120254a09c06d2621b9ec
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2018-10-29 15:45:02 +0100

    Added new exception types

--- special/exceptions.py
+++ special/exceptions.py
@@ -1,2 +1,10 @@
+class PolicyCannotBeViolatedException(Exception):
+    pass
+
+
 class PolicyAspectCannotBeViolatedException(Exception):
+    pass
+
+
+class ParticularPolicyCannotBeViolatedException(Exception):
     pass
